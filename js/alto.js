f5f789f3d37d8b159878a1a2aca1dd6c457173ca
Bralink <30513451+Bralink@users.noreply.github.com>
2017-09-21 12:11:20 -0500

    Add files via upload

---
+++ js/alto.js
@@ -0,0 +1,11 @@
+
+/*ajusta la imagen de fondo 
+a las dimensiones de la pantalla*/
+
+/*SI QUITAMOS COMENTARIOS DEL CODIGO, COMENTAR EL "HEIGHT" EN .contenedor*/
+
+/*$(document).ready(function() {
+	$(".contenedor").css({
+		"height":$(window).height() + "px"
+	});
+});*/
